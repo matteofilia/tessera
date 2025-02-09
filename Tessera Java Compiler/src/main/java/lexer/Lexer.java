2de8c34d6ed0bca92e4bf2898d9de256f515761e
Matteo Filia <matteo@matteofilia.com>
2025-02-08 23:44:24 -0500

    Lexer pseudocode (to be implemented later)

--- Tessera Java Compiler/src/main/java/lexer/Lexer.java
+++ Tessera Java Compiler/src/main/java/lexer/Lexer.java
@@ -1,4 +1,13 @@
 package lexer;
 
 public class Lexer {
+
+    // While input isn't empty:
+    //  if input starts with whitespace:
+    //    trim whitespace from start of input
+    // else:
+    //  find the longest match at start of input for any regex pattern
+    //  if no match is found, raise an error
+    //  convert matching substring into a token
+    //  remove matching substring from start of input
 }
