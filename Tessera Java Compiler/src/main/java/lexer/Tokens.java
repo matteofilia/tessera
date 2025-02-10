eca26c18d972e43616ff52e24784e0b44546068e
Matteo Filia <matteo@matteofilia.com>
2025-02-09 23:41:05 -0500

    Refactor token code

--- Tessera Java Compiler/src/main/java/lexer/Tokens.java
+++ Tessera Java Compiler/src/main/java/lexer/Tokens.java
@@ -1,19 +1,34 @@
 package lexer;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Tokens {
-    public static final Token TEST = new Token(Pattern.compile("test\b"), "TOKEN_TEST");
-    public static final Token IDENTIFIER = new Token(Pattern.compile("[a-zA-Z_]\\w*\\b"), "TOKEN_IDENTIFIER");
-    public static final Token CONSTANT = new Token(Pattern.compile("[0-9]+\\b"), "TOKEN_CONSTANT");
-    public static final Token INT = new Token(Pattern.compile("int\\b"), "TOKEN_INT");
-    public static final Token VOID = new Token(Pattern.compile("void\\b"), "TOKEN_VOID");
-    public static final Token RETURN = new Token(Pattern.compile("return\\b"), "TOKEN_RETURN");
-    public static final Token OPEN_PARENTHESIS = new Token(Pattern.compile("\\("), "TOKEN_OPEN_PARENTHESIS");
-    public static final Token CLOSE_PARENTHESIS = new Token(Pattern.compile("\\)"), "TOKEN_CLOSE_PARENTHESIS");
-    public static final Token OPEN_BRACE = new Token(Pattern.compile("\\{"), "TOKEN_OPEN_BRACE");
-    public static final Token CLOSE_BRACE = new Token(Pattern.compile("\\}"), "TOKEN_CLOSE_BRACE");
-    public static final Token SEMICOLON = new Token(Pattern.compile(";"), "TOKEN_SEMICOLON");
+
+
+    private static final ArrayList<Token> allTokens = new ArrayList<>();
+    
+    public static void init() {
+        Tokens.add(new Token(Pattern.compile("test\b"), "TOKEN_TEST"));
+        Tokens.add(new Token(Pattern.compile("[a-zA-Z_]\\w*\\b"), "TOKEN_IDENTIFIER"));
+        Tokens.add(new Token(Pattern.compile("[0-9]+\\b"), "TOKEN_CONSTANT"));
+        Tokens.add(new Token(Pattern.compile("int\\b"), "TOKEN_INT"));
+        Tokens.add(new Token(Pattern.compile("void\\b"), "TOKEN_VOID"));
+        Tokens.add(new Token(Pattern.compile("return\\b"), "TOKEN_RETURN"));
+        Tokens.add(new Token(Pattern.compile("\\("), "TOKEN_OPEN_PARENTHESIS"));
+        Tokens.add(new Token(Pattern.compile("\\)"), "TOKEN_CLOSE_PARENTHESIS"));
+        Tokens.add(new Token(Pattern.compile("\\{"), "TOKEN_OPEN_BRACE"));
+        Tokens.add(new Token(Pattern.compile("\\}"), "TOKEN_CLOSE_BRACE"));
+        Tokens.add(new Token(Pattern.compile(";"), "TOKEN_SEMICOLON"));
+    }
+
+    public static void add(Token token) {
+        allTokens.add(token);
+    }
+
+    public static ArrayList<Token> getAll() {
+        return allTokens;
+    }
 
     private Tokens() throws Exception {
         throw new Exception("Not Allowed");
