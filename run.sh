385976c9235d57422c73e3ea7c7a731b05a38d5e
Matteo Filia <matteo@matteofilia.com>
2025-02-09 20:43:20 -0500

    Update run script

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-set -o xtrace
-echo "Starting program..."
+# set -o xtrace
+
 echo "Valid arguments: --lex --parse --codegen"
+
+for i in $@;
+do
+	if [[ $i == "--lex" ]]; then
+		echo "Only running lexer"
+	elif [[ $i == "--parse" ]]; then
+		echo "Only running lexer and parser"
+	elif [[ $i == "--codegen" ]]; then
+		echo "Only running lexer, parser, and assembler"
+	fi
+done
+
+echo "Compiling Java using Ant..."
+# ant TODO
+
+echo "Starting program..."
 
 java -jar main.jar "$@"
