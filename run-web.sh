68b41d09cfbaec0a73485f2380ab9573928aa4aa
Matteo Filia <matteo@matteofilia.com>
2025-02-11 17:45:45 -0500

    Add script to run website

---
+++ run-web.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Script Location: $0"
+DIR="$(dirname $0)"
+
+cd "$DIR"
+cp "Tessera Java Compiler/build/jar/main.jar" "$DIR/Tessera Website/java/main.jar"
+
+cd "Tessera Website"
+python3 -m http.server
