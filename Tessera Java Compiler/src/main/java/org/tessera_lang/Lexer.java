04e37c45a7c1e2d1929c3c62be963b964354b9db
Matteo Filia <matteo@matteofilia.com>
2025-02-12 16:32:10 -0500

    Add lexer output to website

--- Tessera Java Compiler/src/main/java/org/tessera_lang/Lexer.java
+++ Tessera Java Compiler/src/main/java/org/tessera_lang/Lexer.java
@@ -1,33 +1,50 @@
 package org.tessera_lang;
 
+import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Lexer {
 
-    public static void lexFile(String inputFile, String outputFile) throws LexerException {
+    public static String lexText(String input) throws LexerException {
         StringBuilder output = new StringBuilder();
+
+        // TODO: fix this problem, please! Should be splitting on newlines
+        String[] lines = input.split("\n");
+        for (String line : lines) {
+            line = line.trim();
+
+            if (Main.BE_VERBOSE) System.out.println("Checking line: "+line);
+            for (LexerToken token : LexerTokens.checkMatches(line)) {
+                // Match Found
+                output.append(token.getId());
+                output.append(" ");
+            }
+        }
+
+        return output.toString();
+    }
+
+    public static void lexFile(String inputFile, String outputFile) throws LexerException {
+        StringBuilder input = new StringBuilder();
+        String output = "";
 
         try {
             FileReader fileReader = new FileReader(inputFile);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+
             try {
                 String line = bufferedReader.readLine();
                 while (line != null) {
-                    line = line.trim();
-
-                    for (LexerToken token : LexerTokens.checkMatches(line)) {
-                        // Match Found
-                        output.append(token.getId());
-                        output.append(" ");
-                    }
-
+                    input.append(line);
                     line = bufferedReader.readLine();
                 }
-
-                bufferedReader.close();
             } catch (IOException e){
                 System.out.println("Error: IO Exception");
             }
+
+            output = lexText(input.toString());
 
         } catch (FileNotFoundException e) {
             System.out.println("Error: file not found");
@@ -35,7 +52,7 @@ public class Lexer {
 
         try {
             Writer writer = new FileWriter(outputFile);
-            writer.write(output.toString());
+            writer.write(output);
             writer.close();;
         } catch (IOException e) {
             System.out.println("Error: IO Exception");
--- Tessera Server/src/main/java/org/tessera_lang/server/TesseraServer/TestController.java
+++ Tessera Server/src/main/java/org/tessera_lang/server/TesseraServer/TestController.java
@@ -1,8 +1,8 @@
 package org.tessera_lang.server.TesseraServer;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.tessera_lang.Lexer;
+import org.tessera_lang.LexerException;
 
 @RestController
 public class TestController {
@@ -13,8 +13,14 @@ public class TestController {
     }
 
     @CrossOrigin(origins = "http://localhost:8000")
-    @GetMapping("/lex")
-    public String lex() {
-        return "(TODO: Lexer Output Goes Here)";
+    @PostMapping("/lex")
+    public String lex(@RequestBody String input) {
+        String output = ";";
+        try {
+            output = Lexer.lexText(input);
+        } catch (LexerException e) {
+            return "Error: LexerException";
+        }
+        return output;
     }
 }
