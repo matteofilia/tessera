b3aa2aa9d47a23b272e57f11ebdcdb5f3df67f02
Matteo Filia <matteo@matteofilia.com>
2025-08-28 18:48:20 -0400

    Update run-server.sh script

--- run-server.sh
+++ run-server.sh
@@ -3,8 +3,12 @@
 echo "Script Location: $0"
 DIR="$(dirname $0)"
 
-export TESSERA_WEB_HOST="http://16.52.132.40"
-export TESSERA_WEB_HOST_B="http://www.tessera-lang.org"
+# Set this to the location of the web host
+export TESSERA_WEB_HOST="http://localhost"
+export TESSERA_WEB_HOST_B="http://localhost:8000"
+
+echo "Using $TESSERA_WEB_HOST..."
+echo "Using $TESSERA_WEB_HOST_B..."
 
 cd "$DIR"
 cp "Tessera Java Interpreter/build/jar/main.jar" "Tessera Server/src/main/resources/main.jar"
