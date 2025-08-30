f645af39d54f141c15c23263eb8fee4d5d77d222
Matteo Filia <matteo@matteofilia.com>
2025-08-29 23:57:58 -0400

    Add test script

---
+++ test.sh
@@ -0,0 +1,17 @@
+echo "Running tests..."
+
+./run.sh code.tess
+
+./run.sh code2.tess
+
+./run.sh code3.tess
+
+./run.sh code4.tess
+
+./run.sh code5.tess
+
+./run.sh code6.tess
+
+./run.sh code7.tess
+
+./run.sh code8.tess
