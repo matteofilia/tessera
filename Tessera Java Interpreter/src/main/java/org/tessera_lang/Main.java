13979f631d4e5e9acddfb261c7a3036869233890
Matteo Filia <matteo@matteofilia.com>
2025-09-02 13:55:39 -0400

    Creating RunConfiguration to run interpreter either using command line or via the website. This should be slighly more elegant, less sloppy

--- Tessera Java Interpreter/src/main/java/org/tessera_lang/Main.java
+++ Tessera Java Interpreter/src/main/java/org/tessera_lang/Main.java
@@ -11,17 +11,10 @@ import org.tessera_lang.parser.ParserASTNode;
 import org.tessera_lang.parser.ParserDebugger;
 import org.tessera_lang.parser.ParserException;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-
-    public static boolean BE_VERBOSE = false;
-    public static boolean BE_VERY_VERBOSE = false;
-    public static boolean WEB = false;
 
     public static final int CODE_OK = 0;
     public static final int CODE_FAIL = 1;
@@ -38,120 +31,92 @@ public class Main {
         return "Thank you for programming with Tessera, "+name;
     }
 
-    private static void printCoolAsciiThing() {
+    private static void printCoolAsciiThing(PrintStream out) {
         String name = "user";
 
-        System.out.println(asciiArt);
-        System.out.println(generateThankYouMessage(name));
+        out.println(asciiArt);
+        out.println(generateThankYouMessage(name));
     }
 
-    private static void printHelp() {
-        System.out.println("");
-        System.out.println("*** TESSERA INTERPRETER HELP ***");
-        System.out.println("");
-        System.out.println("Arguments: ");
-        System.out.println("  -l --lex --- Run Lexer ONLY");
-        System.out.println("  -p --parse --- Run Lexer AND Parser");
-        System.out.println("  -i --interpreter --- Run Lexer AND Parser AND Interpreter");
-        System.out.println("  -w --web --- Run In Web Mode");
-        System.out.println("  -c --code --- Print Out Code (For Reference)");
-        System.out.println("");
-        System.out.println("  -h --help --- Display Help");
-        System.out.println("  -v --verbose --- Display Output Verbosely");
-        System.out.println("  -vv --very-verbose --- Display Output Very Verbosely");
+    private static void printHelp(PrintStream out) {
+        out.println("");
+        out.println("*** TESSERA INTERPRETER HELP ***");
+        out.println("");
+        out.println("Arguments: ");
+        out.println("  -l --lex --- Run Lexer ONLY");
+        out.println("  -p --parse --- Run Lexer AND Parser");
+        out.println("  -i --interpreter --- Run Lexer AND Parser AND Interpreter");
+        out.println("  -w --web --- Run In Web Mode");
+        out.println("  -c --code --- Print Out Code (For Reference)");
+        out.println("");
+        out.println("  -h --help --- Display Help");
+        out.println("  -v --verbose --- Display Output Verbosely");
+        out.println("  -vv --very-verbose --- Display Output Very Verbosely");
 
-        System.out.println("");
-        System.out.println("About: ");
-        System.out.println("  Tessera is an interpreted language, with the interpreter being written in Java.");
+        out.println("");
+        out.println("About: ");
+        out.println("  Tessera is an interpreted language, with the interpreter being written in Java.");
 
-        System.out.println("");
-        System.out.println("Stages: ");
-        System.out.println("  1 - Run Lexer");
-        System.out.println("  2 - Run Parser");
-        System.out.println("  3 - Run Interpreter");
+        out.println("");
+        out.println("Stages: ");
+        out.println("  1 - Run Lexer");
+        out.println("  2 - Run Parser");
+        out.println("  3 - Run Interpreter");
 
-        System.out.println("");
-        System.out.println("Files: ");
-        System.out.println("  Default Lexer File: code.tess");
-        System.out.println("  Default Parser File: tokens.t");
-        System.out.println("  Default Interpreter File: N/A");
+        out.println("");
+        out.println("Files: ");
+        out.println("  Default Lexer File: code.tess");
+        out.println("  Default Parser File: tokens.t");
+        out.println("  Default Interpreter File: N/A");
 
-        System.out.println("");
-        System.out.println("Examples: ");
-        System.out.println("  ./tessera file.tess");
-        System.out.println("  ./tessera --lex --verbose file2.tess");
-        System.out.println("");
+        out.println("");
+        out.println("Examples: ");
+        out.println("  ./tessera file.tess");
+        out.println("  ./tessera --lex --verbose file2.tess");
+        out.println("");
     }
 
-    // TODO: this entire thing is terrible... fix please
+    public static void run(PrintStream out, RunConfiguration runConfig) {
 
-    public static void main(String[] args){
+        if (runConfig.shouldBeVerbose()) {
+            printCoolAsciiThing(out);
+        }
 
-        boolean runLexer = false;
-        boolean runParser = false;
-        boolean runInterpreter = false;
+        if (!runConfig.isWeb()) {
+            out.println("For help, please use -h or --help");
+        }
 
-        boolean printCode = false;
+        if (runConfig.shouldBeVerbose()) {
+            out.println("Using file: "+runConfig.getLexerInputFile());
 
-        String lexerInputFile = "code.tess";
-        String parserInputFile = "tokens.t";
+            out.println("Output mode: verbose");
 
-        printCoolAsciiThing();
-        System.out.println("For help, please use -h or --help");
-
-        for (String arg : args) {
-            if (arg.equals("-l") || arg.equals("--lex")) {
-                // Run Lexer ONLY
-                runLexer = true;
-            } else if (arg.equals("-p") || arg.equals("--parse")) {
-                // Run Lexer AND Parser
-                runLexer = true;
-                runParser = true;
-            } else if (arg.equals("-i") || arg.equals("--interpreter")) {
-                // Run Lexer AND Parser AND Interpreter
-                runLexer = true;
-                runParser = true;
-                runInterpreter = true;
-            } else if (arg.equals("-c") || arg.equals("--code")) {
-                printCode = true;
-            } else if (arg.equals("-v") || arg.equals("--verbose")) {
-                Main.BE_VERBOSE = true;
-                System.out.println("Output mode: verbose");
-            } else if (arg.equals("-vv") || arg.equals("--very-verbose")) {
-                Main.BE_VERBOSE = true;
-                Main.BE_VERY_VERBOSE = true;
-                System.out.println("Output mode: very verbose");
-            } else if (arg.equals("-w") || arg.equals(("--web"))) {
-                Main.WEB = true;
-                if (Main.BE_VERBOSE) System.out.println("Running in web mode");
-            } else if (arg.equals("-h") || arg.equals("--help")) {
-                printHelp();
-            } else {
-                String file = arg;
-                System.out.println("Using file: "+file);
-                lexerInputFile = file;
+            if (runConfig.shouldBeVeryVerbose()) {
+                out.println("Output mode: very verbose");
             }
         }
 
-        if (!runLexer && !runParser && !runInterpreter) {
-            System.out.println("No Action Specified");
-            System.out.println("Running Lexer AND Parser AND Interpreter");
+        if (!runConfig.shouldRunLexer() && !runConfig.shouldRunParser() && !runConfig.shouldRunInterpreter()) {
+            if (runConfig.shouldBeVerbose()) {
+                out.println("No Action Specified");
+                out.println("Running Lexer AND Parser AND Interpreter");
+            }
 
-            runLexer = true;
-            runParser = true;
-            runInterpreter = true;
+            runConfig.setRunLexer(true);
+            runConfig.setRunParser(true);
+            runConfig.setRunInterpreter(true);
         }
 
         ArrayList<LexerToken> lexerList = new ArrayList<>();
         ArrayList<ParserASTNode> trees = new ArrayList<>();
 
-        if (printCode) {
+        if (runConfig.shouldPrintCode()) {
             StringBuilder input = new StringBuilder();
-            System.out.println("");
-            System.out.println("CODE " + "(" + lexerInputFile + ")");
+            out.println("");
+            out.println("CODE " + "(" + runConfig.getLexerInputFile() + ")");
 
             try {
-                FileReader fileReader = new FileReader(lexerInputFile);
+                FileReader fileReader = new FileReader(runConfig.getLexerInputFile());
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
 
                 try {
@@ -161,73 +126,111 @@ public class Main {
                         line = bufferedReader.readLine();
                     }
 
-                    System.out.println(input.toString());
+                    out.println(input.toString());
 
                 } catch (IOException e) {
-                    System.out.println("Error: IO Exception");
+                    out.println("Error: IO Exception");
                     System.exit(CODE_FAIL);
                 }
             } catch (FileNotFoundException e) {
-                System.out.println(e.getMessage());
+                out.println(e.getMessage());
                 System.exit(CODE_FAIL);
             }
         }
 
-        if (runLexer) {
-            System.out.println("");
-            System.out.println("RUNNING LEXER");
-            System.out.println("Running org.tessera_lang.lexer.Lexer: " + lexerInputFile + " -> " + parserInputFile);
+        if (runConfig.shouldRunLexer()) {
+            out.println("");
+            out.println("RUNNING LEXER");
+            out.println("Running org.tessera_lang.lexer.Lexer: " + runConfig.getLexerInputFile() + " -> " + runConfig.getParserInputFile());
             try {
-                lexerList = Lexer.lexFile(lexerInputFile, parserInputFile);
+                lexerList = Lexer.lexFile(runConfig.getLexerInputFile(), runConfig.getParserInputFile());
 
-                if (Main.BE_VERBOSE) {
-                    System.out.print("Lexer Output: ");
+                if (runConfig.shouldBeVerbose()) {
+                    out.print("Lexer Output: ");
                     LexerDebugger.debugLexerTokenList(lexerList);
                 }
             } catch (LexerException e) {
-                System.out.println("Lexer Failure");
+                out.println("Lexer Failure");
                 System.exit(CODE_FAIL);
             }
         }
-        if (runParser) {
-            System.out.println("");
-            System.out.println("RUNNING PARSER");
-            System.out.println("Running org.tessera_lang.parser.Parser: "+parserInputFile);
+        if (runConfig.shouldRunParser()) {
+            out.println("");
+            out.println("RUNNING PARSER");
+            out.println("Running org.tessera_lang.parser.Parser: "+runConfig.getParserInputFile());
             try {
-                 trees = Parser.parse(lexerList);
+                trees = Parser.parse(lexerList);
 
                 for (ParserASTNode head : trees) {
-                    if  (Main.BE_VERBOSE) {
+                    if  (runConfig.shouldBeVerbose()) {
                         ParserDebugger.printDebugOutput(head);
                     }
 
-                    if (head == null && Main.BE_VERBOSE) {
-                        System.out.println("Head is null!");
+                    if (head == null && runConfig.shouldBeVerbose()) {
+                        out.println("Head is null!");
                     }
 
-                    if (Main.BE_VERBOSE) {
+                    if (runConfig.shouldBeVeryVerbose()) {
                         ParserDebugger.printDebugTraversal(Parser.getHeadRecursive(head));
                     }
                 }
             } catch (ParserException e) {
-                System.out.println("Parser Failure");
+                out.println("Parser Failure");
                 System.exit(CODE_FAIL);
             }
         }
 
         // Run Interpreter
-        if (runInterpreter) {
-            System.out.println("");
-            System.out.println("RUNNING INTERPRETER");
+        if (runConfig.shouldRunInterpreter()) {
+            out.println("");
+            out.println("RUNNING INTERPRETER");
 
             try {
                 Interpreter.run(trees, System.out);
             } catch (InterpreterException e) {
-                System.out.println(e.getMessage());
+                out.println(e.getMessage());
                 System.exit(CODE_FAIL);
             }
         }
 
         System.exit(CODE_OK);
+    }
+
+    public static void main(String[] args){
+        RunConfiguration runConfig = new RunConfiguration();
+        run(System.out, runConfig);
+
+
+        // Parse Args
+        for (String arg : args) {
+            if (arg.equals("-l") || arg.equals("--lex")) {
+                // Run Lexer ONLY
+                runConfig.setRunLexer(true);
+            } else if (arg.equals("-p") || arg.equals("--parse")) {
+                // Run Lexer AND Parser
+                runConfig.setRunLexer(true);
+                runConfig.setRunParser(true);
+            } else if (arg.equals("-i") || arg.equals("--interpreter")) {
+                // Run Lexer AND Parser AND Interpreter
+                runConfig.setRunLexer(true);
+                runConfig.setRunParser(true);
+                runConfig.setRunInterpreter(true);
+            } else if (arg.equals("-c") || arg.equals("--code")) {
+                runConfig.setPrintCode(true);
+            } else if (arg.equals("-v") || arg.equals("--verbose")) {
+                runConfig.setBeVerbose(true);
+            } else if (arg.equals("-vv") || arg.equals("--very-verbose")) {
+                runConfig.setBeVerbose(true);
+                runConfig.setBeVeryVerbose(true);
+            } else if (arg.equals("-w") || arg.equals(("--web"))) {
+                runConfig.setWeb(true);
+
+            } else if (arg.equals("-h") || arg.equals("--help")) {
+                printHelp(System.out);
+            } else {
+                String file = arg;
+                runConfig.setLexerInputFile(file);
+            }
+        }
     }
 }
