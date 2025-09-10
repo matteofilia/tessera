e6c38d1bcb7144ffacc4b0f239814d02cf237e4b
Matteo Filia <matteo@matteofilia.com>
2025-09-09 21:36:59 -0400

    Fix things and also add junit sample tests

---
+++ Tessera Java Interpreter/src/test/java/TesseraTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TesseraTests {
+
+    @Test
+    void exampleTest() {
+        assertEquals(420, 420);
+    }
+
+    @Test
+    void exampleTest2() {
+        assertEquals(1, 1);
+    }
+}
