4abe8a1c01418dad990a477172645e00da872502
Matteo Filia <matteo@matteofilia.com>
2025-09-13 15:26:20 -0400

    Remove automatic copying of files, for now

--- run-web.sh
+++ run-web.sh
@@ -7,7 +7,7 @@ cd "$DIR"
 
 echo "Copying demo code files to website..."
 echo "Demo code files should be named example1.tess or example7.tess"
-cp 'Tessera Demo Code'/*.tess 'Tessera Website'/public/code_examples
+# cp 'Tessera Demo Code'/*.tess 'Tessera Website'/public/code_examples
 
 cd "Tessera Website"
 python3 -m http.server
